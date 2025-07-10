3c80b72a060190322cca2213715f624d95cfc580
ilaydad <ilayda.dincer@bilgiedu.net>
2025-07-10 10:00:19 +0300

    making properties file name a command-line argument

---
+++ src/hellojava/DatabaseReader.java
@@ -0,0 +1,54 @@
+package hellojava;
+
+import java.sql.*;
+import java.util.Properties;
+import java.io.InputStream;
+import java.io.IOException;
+
+public class DatabaseReader {
+	public static void main (String[] args) {
+		Properties props = new Properties();
+		
+		try {
+			if (args.length < 1) {
+				System.out.println("please enter a .properties file name.");
+				return;
+			}
+			
+        InputStream input =  DatabaseReader.class.getClassLoader().getResourceAsStream(args[0]);
+        
+        if (input == null) {
+        	System.out.println(args [0] + "file does not exist." );
+        	return;
+        	}
+        
+        
+        props.load(input);
+        
+        String url = props.getProperty("url");
+        String user = props.getProperty("user");
+        String password = props.getProperty("password");
+        
+        Connection conn = DriverManager.getConnection(url,user,password);
+        Statement stmt = conn.createStatement();
+        
+        String sql = "SELECT * FROM students";
+        ResultSet rs = stmt.executeQuery(sql);
+        while (rs.next()) {
+        	System.out.println("ID:" + rs.getInt("id")
+        	+" , Name : " + rs.getString("name")
+        	+" , Age: " + rs.getInt("age"));
+        }
+        
+        rs.close();
+        stmt.close();
+        conn.close();
+        
+		  } catch (IOException | SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	}
+
+	
+        
