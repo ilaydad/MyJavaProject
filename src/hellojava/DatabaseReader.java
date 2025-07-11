40857fe2caab4e3f8c3e9bce8f29875cdb7a739e
ilaydad <ilayda.dincer@bilgiedu.net>
2025-07-11 11:09:10 +0300

    Refactoring: Splitting DatabaseReader into configuration, connection, and query methods

--- src/hellojava/DatabaseReader.java
+++ src/hellojava/DatabaseReader.java
@@ -9,49 +9,67 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 public class DatabaseReader {
+
     public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("please enter a .properties file name.");
+            return;
+        }
+
+        Properties props = loadProperties(args[0]);
+        if (props == null) return;
+
+        HikariDataSource dataSource = createDataSource(props);
+        if (dataSource == null) return;
+
+        readStudents(dataSource);
+
+        dataSource.close();
+    }
+
+    private static Properties loadProperties(String fileName) {
         Properties props = new Properties();
-
-        try {
-            if (args.length < 1) {
-                System.out.println("Please enter a .properties file name.");
-                return;
-            }
-
-            InputStream input = DatabaseReader.class.getClassLoader().getResourceAsStream(args[0]);
+        try (InputStream input = DatabaseReader.class.getClassLoader().getResourceAsStream(fileName)) {
             if (input == null) {
-                System.out.println(args[0] + " file does not exist.");
-                return;
+                System.out.println(fileName + " file does not exist.");
+                return null;
             }
-
             props.load(input);
+            return props;
+        } catch (IOException e) {
+            System.out.println("IO error: " + e.getMessage());
+            return null;
+        }
+    }
 
-            // Hikari config
+    private static HikariDataSource createDataSource(Properties props) {
+        try {
             HikariConfig config = new HikariConfig();
             config.setJdbcUrl(props.getProperty("jdbcUrl"));
             config.setUsername(props.getProperty("username"));
             config.setPassword(props.getProperty("password"));
             config.setDriverClassName(props.getProperty("driverClassName"));
 
-            HikariDataSource dataSource = new HikariDataSource(config);
+            return new HikariDataSource(config);
+        } catch (Exception e) {
+            System.out.println("Failed to create DataSource: " + e.getMessage());
+            return null;
+        }
+    }
 
+    private static void readStudents(HikariDataSource dataSource) {
+        try (
             Connection conn = dataSource.getConnection();
             Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM students");
-
+            ResultSet rs = stmt.executeQuery("SELECT * FROM students")
+        ) {
             while (rs.next()) {
                 System.out.println("ID: " + rs.getInt("id") +
                                    ", Name: " + rs.getString("name") +
                                    ", Age: " + rs.getInt("age"));
             }
-
-            rs.close();
-            stmt.close();
-            conn.close();
-            dataSource.close();
-
-        } catch (IOException | SQLException e) {
-            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("SQL error: " + e.getMessage());
         }
     }
-} 
+}
