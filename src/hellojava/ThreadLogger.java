4f88e067e4fc0d4f199befcfc2d7ec5868c3f76b
ilaydad <ilayda.dincer@bilgiedu.net>
2025-07-14 15:12:49 +0300

    Added ThreadLogger example using multithreading

---
+++ src/hellojava/ThreadLogger.java
@@ -0,0 +1,22 @@
+package hellojava;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ThreadLogger implements Runnable  {
+	private static final Logger logger = LogManager.getLogger(ThreadLogger.class);
+	 public static void main(String[] args) {
+	        Thread thread = new Thread(new ThreadLogger());
+	        thread.start();
+	    }
+
+	    @Override
+	    public void run() {
+	        logger.info("Thread is working — log entry written by a separate thread.");
+	        System.out.println("thread is running and writing to log file.");
+	    }
+	}
+
+	
+
+
